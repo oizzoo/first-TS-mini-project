251ae7a124c86f439eb47eb12e2f9ec250e1de67
oizzoo <108180322+oizzoo@users.noreply.github.com>
2025-11-27 16:41:22 +0100

    first commit

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,14 @@
+type CourseGoalProps = {
+    title: string;
+    description: string;
+}
+
+export default function CourseGoal({title, description}: CourseGoalProps) {
+    return <article>
+        <div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+        <button>Delete</button>
+    </article>
+};
