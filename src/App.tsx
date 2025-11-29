484fc77c81078158d5e9152996ab56f105c3f6d6
oizzoo <108180322+oizzoo@users.noreply.github.com>
2025-11-29 23:19:55 +0100

    feat: add NewGoal.tsx file, create logic to be able to type in your goal and summary instead of having dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import Header from './components/Header';
 import goalsImg from './assets/goals.jpg';
 import CourseGoalList from './components/CourseGoalList'
+import NewGoal from './components/NewGoal';
 
 export type CourseGoal = {
   title: string;
@@ -12,13 +13,13 @@ export type CourseGoal = {
 export default function App() {
   const [goals, setGoals] = useState<CourseGoal[]>([]);
 
-  function handleAddGoal() {
+  function handleAddGoal(goal: string, summary: string) {
     setGoals(prevGoals => {
 
       const newGoal: CourseGoal = {
         id: Math.random(),
-        title: 'Learn React + TS',
-        description: 'Learn it in depth!'
+        title: goal,
+        description: summary,
       }; 
 
       return [...prevGoals, newGoal]
@@ -34,7 +35,7 @@ export default function App() {
       <Header image={{src: goalsImg, alt: 'A list of goals'}}>
         <h1>Your Course Goals</h1>
       </Header>
-      <button onClick={handleAddGoal}>Add Goal</button>
+      <NewGoal onAddGoal={handleAddGoal}/>
       <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal}/>
     </main>
   )
